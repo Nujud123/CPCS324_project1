64a45a709150960d419d515da668e09678f196e6
rabbitRM <102517158+rabbitRM@users.noreply.github.com>
2023-05-10 23:34:23 +0300

    Add files via upload

---
+++ GraphFramework/MSTAlgorithm.java
@@ -0,0 +1,20 @@
+package GraphFramework;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+
+public abstract class MSTAlgorithm {
+    
+    
+    public  ArrayList<GraphFramework.Edge> MSTResultList =  new ArrayList<GraphFramework.Edge>();
+    
+    // ======== I dont know what to do with this (:
+    Graph graph  ;
+   
+    
+    public abstract void displayResultingMST( ArrayList<GraphFramework.Edge> MSTResultList);
+        
+    
+}
